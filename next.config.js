2e07624069a6c26e57c1712f46095399715af8aa
Conar Welsh <conarw@gmail.com>
2021-09-23 12:11:15 -0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,1 @@
+module.exports = { distDir: ".dist/.next" }
---
+++ src/main/index.js
@@ -0,0 +1,31 @@
+const path = require("path")
+const url = require("url")
+
+const { BrowserWindow, app, ipcMain } = require("electron")
+const isDev = require("electron-is-dev")
+const prepareNext = require("electron-next")
+
+app.on("window-all-closed", app.quit)
+
+app.on("ready", async () => {
+  await prepareNext("./src")
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  })
+
+  const url = isDev
+    ? "http://localhost:8000"
+    : url.format({
+        pathname: path.join(__dirname, "../../dist/index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+
+  mainWindow.loadURL(url)
+})
